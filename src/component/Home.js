dab8910247b620125e549f8d1daee2c5e40c6851
HamaylAfzal <hamaylafzal_2023@depauw.edu>
2020-08-08 15:27:22 -0400

    added links

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import Github from '../Pictures/github.png'
+import Linkdin from '../Pictures/linkdin.png'
+import Gmail from '../Pictures/gmail.png'
 
 
-function Home(){
+
+export class Home extends Component {
+  render() {
     return (
-        <div>
-            <h1> Home page </h1>
-        </div>
-
-    )
+      <div>
+        <a
+          href="https://github.com/hamaylafzal"
+          target="_https://github.com/hamaylafzal"
+        >
+          <img src={Github} alt="github link" />
+        </a>
+        <a
+          href="http://www.linkedin.com/in/hamayl-afzal"
+          target="_http://www.linkedin.com/in/hamayl-afzal"
+        >
+          <img src={Linkdin} alt="Linkdin" />
+        </a>
+        <a
+          href="hamaylafzal_2023@depauw.edu"
+          target="_hamaylafzal_@023@depauw.edu"
+        >
+          <img src={Gmail} alt="gmail" />
+        </a>
+      </div>
+    );
+  }
 }
+
 export default Home;
